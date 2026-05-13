b732c553c92083c8e371b11cade34c5bff3c25b3
I.AYYAPPA REDDY <iayyappa87@gmail.com>
2026-05-13 22:43:05 +0530

    Refactor Django settings for GENYOGA project

--- GENYOGA/settings.py
+++ GENYOGA/settings.py
@@ -42,11 +42,7 @@ INSTALLED_APPS = [
 # MIDDLEWARE
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-
-    *(
-        ['whitenoise.middleware.WhiteNoiseMiddleware']
-        if not DEBUG else []
-    ),
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 
 ROOT_URLCONF = 'GENYOGA.urls'
 
@@ -122,6 +117,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # MEDIA FILES
