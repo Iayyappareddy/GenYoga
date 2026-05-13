6fa303efdc7acb56496600732e2a7e8fc4570c32
I.AYYAPPA REDDY <iayyappa87@gmail.com>
2026-05-13 19:35:06 +0530

    Add files via upload

--- GENYOGA/settings.py
+++ GENYOGA/settings.py
@@ -80,14 +80,9 @@ WSGI_APPLICATION = 'GENYOGA.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'genyoga',
-        'USER': 'postgres',
-        'PASSWORD': 'A123@ayyappa',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 # Password validation
